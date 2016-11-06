874824c18b74c6382ae2a0d324381d372e024633
Darren Deanto <darrengdeanto@gmail.com>
2016-11-05 22:49:32 -0400

    added node and express

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+
+var indexController = require('./controllers/index.js')
+
+// set controllers (routes)
+indexController(app)
+
+app.use('/public', express.static(__dirname + '/public'));
+
+var port = process.env.PORT || 3000
+console.log('port: ' + port)
+
+var httpServer = require('http').createServer(app);
+httpServer.listen(port, function () {
+	console.log('dog-training running on ' + port + '.');
+})
